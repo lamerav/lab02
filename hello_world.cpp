9aec01ef8805eb14b8f420de9fa589fcfb78bb4b
lamerav <89978103+lamerav@users.noreply.github.com>
2022-03-21 17:13:34 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,5 +5,6 @@ int main(){
 	std::string s;
 	std::cin >> s; // Input from console
 	std::cout << "Hello world!" << name << endl; //Output text with input string
+	// Added to conflict
 	return 0;
 }
