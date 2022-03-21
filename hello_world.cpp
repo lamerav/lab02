472fa9ea8eb1099eae5060b3778004ba1d116179
lamerav <gmbelove@mail.ru>
2022-03-21 17:09:36 +0300

    Added clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 
+
 int main(){
 	std::string s;
 	std::cin >> s; // Input from console
 	std::cout << "Hello world!" << name << endl; //Output text with input string
-	// Added to conflict
+	// Added to conflict.
 	return 0;
 }
